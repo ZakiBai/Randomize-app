4369848a48603ab15739b33a35206a3e2ed069b9
Zaki <menza.zm@gmail.com>
2023-04-04 17:05:12 +0600

    Initial Commit

---
+++ Randomize app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Randomize app
+//
+//  Created by Zaki on 04.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
